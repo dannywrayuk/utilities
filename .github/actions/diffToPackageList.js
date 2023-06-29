9bc9675b9fe79f505848625cb2b9c8ed86ee396a
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2023-06-30 00:29:50 +0100

    🐞

--- .github/actions/diffToPackageList.js
+++ .github/actions/diffToPackageList.js
@@ -5,7 +5,7 @@ const changedFiles = diff.split(",");
 const changedPackages = new Set();
 changedFiles.forEach((file) => {
   const matches = file.match(/^(packages\/[a-z0-9\-]+)/);
-  if (matches[0]) {
+  if (matches?.[0]) {
     changedPackages.add(matches[1]);
   }
 });

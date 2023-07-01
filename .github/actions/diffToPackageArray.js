3f8e652773adc9a4c5985b8c6e71cb04ed2f3add
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2023-07-01 04:50:57 +0100

    👷‍♂️ Testing new github action (#8)

---
+++ .github/actions/diffToPackageArray.js
@@ -0,0 +1,14 @@
+const fs = require("node:fs");
+
+let GIT_DIFF = fs.readFileSync("GIT_DIFF").toString();
+
+const changedFiles = GIT_DIFF.split("\n");
+
+const changedPackages = new Set();
+changedFiles.forEach((file) => {
+  const matches = file.match(/^packages\/([a-z0-9\-]+)/);
+  if (matches?.[0]) {
+    changedPackages.add(matches[1]);
+  }
+});
+console.log("packageArray=" + JSON.stringify(Array.from(changedPackages)));

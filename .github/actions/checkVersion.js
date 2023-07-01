869ce8dc580426b61b610b05a1b3d163bc82cd8e
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2023-07-01 04:42:17 +0100

    Add deploy workflow

---
+++ .github/actions/checkVersion.js
@@ -0,0 +1,42 @@
+const fs = require("node:fs");
+const path = require("node:path");
+
+const LATEST_VERSION = fs.readFileSync("LATEST_VERSION").toString();
+
+const npmResponse = JSON.parse(LATEST_VERSION);
+console.log("NPM Response:\n" + JSON.stringify(npmResponse, null, 2));
+const latestVersion = npmResponse.version;
+
+const packageName = npmResponse.name.replace("@dannywrayuk/", "");
+
+const CURRENT_PACKAGE = fs
+  .readFileSync(path.join("packages", packageName, "package.json"))
+  .toString();
+const packageData = JSON.parse(CURRENT_PACKAGE);
+const currentVersion = packageData.version;
+console.log("Current package version: " + currentVersion);
+
+const latestVersionArray = latestVersion.split(".");
+const currentVersionArray = currentVersion.split(".");
+
+if (
+  Number(currentVersionArray[0]) - Number(latestVersionArray[0]) === 1 ||
+  Number(currentVersionArray[1]) - Number(latestVersionArray[1]) === 1 ||
+  Number(currentVersionArray[2]) - Number(latestVersionArray[2]) === 1
+) {
+  console.log(
+    "Current (" +
+      currentVersion +
+      ") is grater than latest (" +
+      latestVersion +
+      ")"
+  );
+} else {
+  throw new Error(
+    "Current (" +
+      currentVersion +
+      ") is not grater than latest (" +
+      latestVersion +
+      "). Cannot publish a lower version."
+  );
+}
